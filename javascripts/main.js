843c5a60dc5a556acf706a8183895587e8bcbbf7
Gilberto Diaz <gdiaz@Gilbertos-MacBook-Pro.local>
2017-07-17 11:01:42 -0500

    adding content to js object

---
+++ javascripts/main.js
@@ -0,0 +1,50 @@
+const pajamas = [
+    {
+        model: "DTZ27310SA",
+        description: "This fun two-piece women's knit pajama set from Secret Treasures is perfect for lounging around the house on lazy Sundays or relaxing on the sofa for movie nights. Made of a soft cotton blend, this sleepwear set has stylish details and a relaxed fit to make it trendy, durable and functional. The casual top has colorful contrast stitching and short sleeves with a tag-free inner for added comfort. The pants are capri length with a covered elastic waistband. This PJ set is available in assorted colors, designs and plus sizes to fit the style and comfort of every woman.",
+        img_ulr: "img/image-1.jpg",
+        colors: ["blue", "yellow", "brown"],
+        category: "a",
+        gender: "w"
+    },
+    {
+        name: "DTZ97410FA",
+        description: "Enjoy a comfortable night's sleep with this women's two-piece sleepwear set from Secret Treasures. Both a tank and a pair of capris are included in the set, and both are made of soft and stretchy fabric that's cool to the touch to help you stay breezy on hot summer nights. The tank has a set of double spaghetti straps and a racerback with a keyhole for added breathability, while the capris have a hassle-free covered elastic waistband for a customized fit. This sleepwear set is available in both regular and plus sizes.",
+        img_ulr: "img/image-1.jpg",
+        colors: ["blue", "yellow", "brown"],
+        category: "a",
+        gender: "w"
+    },
+    {
+        name: "DTZ72311FT",
+        description: "You'll have sweet dreams when you hit the hay in this Sun, Moon & Stars Women's Rayon Short-Sleeve Sleep Top and Short 2-Piece Sleepwear Set. Made of a super-soft rayon blend, the top of this pajama set has short sleeves and a sexy V-neckline with delicate lace trim and a satin bow attached in the middle. The easy pull-on shorts have a relaxed fit and covered elastic waistband for added comfort. The waist features embroidered trim for a feminine touch that complements the lace trim on the side slits. This soft and comfy sleep gown is available in assorted printed patterns.",
+        img_ulr: "img/image-1.jpg",
+        colors: ["blue", "yellow", "brown"],
+        category: "a",
+        gender: "w"
+    },
+    {
+        name: "FZ486GLLWM",
+        description: "Like a snowflake, I'm one of a kind. Your little one will love this Frozen pajama set. The long sleeve tee, made of 100% polyester, has a large screen print graphic of Elsa with different size and color snowflakes. The fleece pants included in the set are constructed with a soft 100% micro fleece fabric that is sure to keep her warm and cozy as she prepares for bed, also with small graphics of purple and blue snowflakes. Available in sizes 6/6X - 10/12.",
+        img_ulr: "img/image-1.jpg",
+        colors: ["blue", "yellow", "brown"],
+        category: "ch",
+        gender: "w"
+    },
+    {
+        name: "MB008GBSWM",
+        description: "Fall and winter are the perfect seasons to bundle up in this type of full-body sleeper. The soft, micro fleece construction is not only flame resistant, but also great for sleeping and lounging. Featuring her favorite characters from Monster High, this soft micro fleece sleeper will ensure she stays warm and comfy, while she shows off the bright colors and fun prints. Pair with a tank top or tee underneath for additional warmth. Available in assorted colors for more fun looks.",
+        img_ulr: "img/image-1.jpg",
+        colors: ["blue", "yellow", "brown"],
+        category: "ch",
+        gender: "w"
+    },
+    {
+        name: "HK223GBSWM",
+        description: "Make bedtime more fun with this versatile Girls' Blanket Sleeper. These pajamas feature soft polyester fabric, offer stress-free care and are available in an assortment of colorful charming prints. The full front zipper includes a large easy-to-use pull, which allows your little one to dress herself. This character hooded sleeper includes long sleeves and long pants with banded cuffs that help them stay comfortably in place.",
+        img_ulr: "img/image-1.jpg",
+        colors: ["blue", "yellow", "brown"],
+        category: "ch",
+        gender: "w"
+    }
+]
